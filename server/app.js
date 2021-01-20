c5258c5347ee2e842715eeff69c66a8e523048f9
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-01-20 07:58:12 -0600

    react eslint babel init

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.get('/test', function (_req, res) {
+  res.send('Hello World');
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  const staticFiles = express.static(path.join(__dirname, '../client/build'));
+  app.use(staticFiles);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
+function start() {
+  app.listen(port, console.log('started!!!'));
+}
+const _start = start;
+
+export { _start as start };
