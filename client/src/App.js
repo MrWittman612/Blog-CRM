897460cc67ae8024c3270e4c7161177d04b16477
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-01-22 03:28:56 -0600

    add assets folder with image and component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import PageUnderConstruction from './components/atoms/PageUnderConstruction';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>Hello world!!</h1>
-    </div>
-  );
+  return <PageUnderConstruction />;
 }
 
 export default App;
---
+++ client/src/components/atoms/PageUnderConstruction/PageUnderConstruction.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import image from '../../../assets/web-site-under-construction.jpg';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundImage: `url('${image}')`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  },
+}));
+
+export function PageUnderConstruction() {
+  const classes = useStyles();
+  return <div className={classes.root}></div>;
+}
