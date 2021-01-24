cc12183e4799dd7312f6cb2529fab529f335c6bd
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-01-24 11:31:03 -0600

    added Links to admin and news page on the app bar added some grid layout to the body of the page

--- client/src/pages/Blog/Blog.js
+++ client/src/pages/Blog/Blog.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Axios from 'axios';
 
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
-import { StopOutlined } from '@material-ui/icons';
+import {
+  AppBar,
+  CssBaseline,
+  IconButton,
+  Toolbar,
+  Typography,
+  Container,
+  Grid,
+  makeStyles,
+  Button,
+} from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles(() => ({
+  main: { paddingTop: 125 },
+  title: { flexGrow: 1 },
+}));
 
 export function Blog() {
   const [blogs, setBlogs] = React.useState(null);
+  const classes = useStyles();
 
   React.useEffect(() => {
     const getBlogs = async () => {
@@ -21,22 +40,71 @@ export function Blog() {
 
   return (
     <React.Fragment>
+      <CssBaseline />
       <AppBar>
         <Toolbar>
-          <IconButton>
-            <StopOutlined />
+          <IconButton color='inherit' size='medium'>
+            <AccountCircleIcon />
           </IconButton>
-          <Typography>Blog</Typography>
+          <Typography className={classes.title}>Blog</Typography>
+          <Grid container direction='row' justify='flex-end' spacing={3}>
+            <Grid item>
+              <Button
+                color='inherit'
+                variant='outlined'
+                component={Link}
+                to={'/admin'}
+              >
+                Admin
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                color='secondary'
+                variant='contained'
+                component={Link}
+                to={'/newsletter'}
+              >
+                News
+              </Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
-      {!blogs && <h1>Loading blogs</h1>}
-      {blogs &&
-        blogs.map((blog) => (
-          <>
-            <h1>{blog.title}</h1>
-            <p>{blog.text}</p>
-          </>
-        ))}
+      <Container className={classes.main} maxWidth='lg'>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8}>
+            <Typography variant='h3'>Featured Blog</Typography>
+            <Typography variant='p'>
+              Sint velit est enim irure velit cillum enim voluptate duis amet.
+              Occaecat culpa esse laboris enim officia nulla qui ullamco commodo
+              magna nisi. Commodo velit minim deserunt dolor exercitation
+              excepteur officia incididunt tempor exercitation cillum ut ut.
+              Voluptate id cillum qui velit laborum quis ea excepteur velit.
+              Incididunt cupidatat fugiat labore anim aliqua. Ex sunt excepteur
+              ipsum non commodo do officia labore excepteur reprehenderit duis
+              sit nisi.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Grid container direction='column' spacing={3}>
+              <Grid item>
+                <Typography variant='h4'>Recent posts</Typography>
+                {!blogs && <h1>Loading blogs</h1>}
+                {blogs &&
+                  blogs.map((blog) => (
+                    <>
+                      <Typography variant='h5'>{blog.title}</Typography>
+                    </>
+                  ))}
+              </Grid>
+              <Grid item>
+                <Typography variant='h4'>Links</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     </React.Fragment>
   );
 }
